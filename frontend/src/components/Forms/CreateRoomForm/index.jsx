5a28c2cb836c1dce6da9a91a29bc905ed43cf15f
AliHakim773 <alihakim7732@gmail.com>
2023-12-08 13:26:03 +0200

    finished forms styling

--- frontend/src/components/Forms/CreateRoomForm/index.jsx
+++ frontend/src/components/Forms/CreateRoomForm/index.jsx
@@ -3,9 +3,42 @@ import "./styles.css"
 
 const CreateRoomForm = () => {
     return (
-        <div>
-            <h1>create room form</h1>
-        </div>
+        <form className='form col-md-12 mt-5'>
+            <div className='form-group'>
+                <input
+                    type='text'
+                    className='form-control my-2'
+                    placeholder='Enter your name'
+                />
+            </div>
+            <div className='form-group border'>
+                <div className='input-group d-flex align-items-center justify-content-center'>
+                    <input
+                        type='text'
+                        className='form-control my-2 border-0'
+                        disabled
+                        placeholder='Generate room code'
+                    />
+                    <div className='input-group-append'>
+                        <button
+                            className='btn btn-primary btn-sm me-1'
+                            type='button'>
+                            generate
+                        </button>
+                        <button
+                            className='btn btn-outline-danger btn-sm me-2'
+                            type='button'>
+                            copy
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <button
+                type='submit'
+                className='btn btn-primary btn-block mt-4 form-control'>
+                Generate Room
+            </button>
+        </form>
     )
 }
 
--- frontend/src/components/Forms/JoinRoomForm/index.jsx
+++ frontend/src/components/Forms/JoinRoomForm/index.jsx
@@ -2,7 +2,29 @@ import React from "react"
 import "./styles.css"
 
 const JoinRoomForm = () => {
-    return <h1>Join Room Form</h1>
+    return (
+        <form className='form col-md-12 mt-5'>
+            <div className='form-group'>
+                <input
+                    type='text'
+                    className='form-control my-2'
+                    placeholder='Enter your name'
+                />
+            </div>
+            <div className='form-group'>
+                <input
+                    type='text'
+                    className='form-control my-2'
+                    placeholder='Enter room code'
+                />
+            </div>
+            <button
+                type='submit'
+                className='btn btn-primary btn-block mt-4 form-control'>
+                Join Room
+            </button>
+        </form>
+    )
 }
 
 export default JoinRoomForm
