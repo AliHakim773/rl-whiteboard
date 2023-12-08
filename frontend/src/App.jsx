899a6a145b6651924b62e9c9c58c8a9703f71c69
AliHakim773 <alihakim7732@gmail.com>
2023-12-08 12:50:28 +0200

    initialized Forms component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css"
+import Forms from "./components/Forms"
 
 const App = () => {
     return (
         <div className='app'>
-            <h1>Hello world</h1>
+            <Forms />
         </div>
     )
 }
---
+++ frontend/src/components/Forms/index.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "./styles.css"
+
+const Forms = () => {
+    return <div>Forms Working</div>
+}
+
+export default Forms
